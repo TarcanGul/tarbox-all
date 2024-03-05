7d59f0446d33bf233f25b21908cb855d3e5e65c1
Tarcan Gul <nailtarcan@gmail.com>
2024-03-04 21:17:02 -0500

    Merge all web views to one web controller.

--- tarbox/src/main/java/tarcan/projects/tarbox/controllers/MainController.java
+++ tarbox/src/main/java/tarcan/projects/tarbox/controllers/MainController.java
@@ -1,14 +1,33 @@
 package tarcan.projects.tarbox.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import tarcan.projects.tarbox.repositories.GameRepository;
 
+/**
+ * Web controller for the web views.
+ */
 @Controller
 public class MainController {
+
+    @Autowired
+    GameRepository gameRepository;
 
     @GetMapping("/")
     public String main() {  
         return "home";
+    }
+
+    @GetMapping("/game/{gameId}")
+    public String getGameView(Model model, @PathVariable Long gameId) {
+        if(!gameRepository.existsById(gameId)) {
+            return "home";
+        }
+        model.addAttribute("gameId", gameId);
+        return "game";
     }
 }
