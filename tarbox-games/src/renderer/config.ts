51ff62855436cb833edf52ab70db257639899004
Tarcan Gul <nailtarcan@gmail.com>
2024-03-27 20:53:00 -0400

    small updates after first release

--- tarbox-games/src/renderer/config.ts
+++ tarbox-games/src/renderer/config.ts
@@ -1,6 +1,6 @@
 const tarboxAppConfig = {
-    tarboxWebsocketURL: new URL("ws://localhost:8080/ws"),
-    tarboxServerUrl: new URL("http://localhost:8080")
+    tarboxWebsocketURL: new URL("ws://ec2-52-91-165-30.compute-1.amazonaws.com/ws"),
+    tarboxServerUrl: new URL("http://ec2-52-91-165-30.compute-1.amazonaws.com")
 }
 
 export default tarboxAppConfig;
